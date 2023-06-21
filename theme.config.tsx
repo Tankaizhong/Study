a8e666fc4970ddb7a9c9fceaff4f89c66f37da9f
Tankaizhong <2534658839@qq.com>
2023-06-21 13:10:53 +0800

    增加入口导航

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>首页</span>,
-  project: {
-    link: "https://github.com/shuding/nextra-docs-template",
-  },
-  chat: {
-    link: "https://discord.com",
-  },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  logo: <span>导航栏  
+    
+    ➡️</span>,
+  // project: {
+  //   link: "https://github.com/shuding/nextra-docs-template",
+  // },
+  // chat: {
+  //   link: "https://discord.com",
+  // },
+  // docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: "Nextra Docs Template",
+    text: "2022.6.31",
   },
   i18n: [{ locale: "zh", text: "中文" }],
 };
